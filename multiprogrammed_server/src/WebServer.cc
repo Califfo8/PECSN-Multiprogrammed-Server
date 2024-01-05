7e76451d59f7d53fda80ad499e7e2e25cf8d41e6
Gabriele Suma <g.suma@studenti.unipi.it>
2024-01-05 18:33:37 +0100

    Implementing object transaction for WebServer

--- multiprogrammed_server/src/WebServer.cc
+++ multiprogrammed_server/src/WebServer.cc
@@ -24,20 +24,20 @@ void WebServer::initialize(){
     qs_rate_ = par("web_server_rate");
 
 }
-void WebServer::elaborate_self_msg_(cMessage * msg){
+void WebServer::elaborate_self_msg_(Transaction * msg){
     msg->setName("QS_to_CPU");
     send(msg,"out");
     if(qs_queue_->isEmpty()){
         working_ = false;
     }
     else {
-        cMessage *self = check_and_cast <cMessage* > (qs_queue_->pop());
+        Transaction *self = check_and_cast <Transaction* > (qs_queue_->pop());
         scheduleAt(simTime()+ exponential( 1 / qs_rate_ , 0 ), self);
         working_ = true;
     }
 }
 
-void WebServer::elaborate_external_msg_(cMessage * msg){
+void WebServer::elaborate_external_msg_(Transaction * msg){
     if(!working_){
         scheduleAt(simTime() + exponential( 1 / qs_rate_ , 0 ) , msg );
         working_ = true;
@@ -47,7 +47,7 @@ void WebServer::elaborate_external_msg_(cMessage * msg){
     }
 }
 
-void WebServer::handleMessage(cMessage * msg){
+void WebServer::handleMessage(Transaction * msg){
    if (msg->isSelfMessage() ){
         elaborate_self_msg_(msg);
     }else{
@@ -58,7 +58,7 @@ void WebServer::handleMessage(cMessage * msg){
 
 void WebServer::finish(){
     while(!qs_queue_->isEmpty()){
-        cMessage *cleaning = check_and_cast<cMessage*>(qs_queue_->pop());
+        Transaction *cleaning = check_and_cast<Transaction*>(qs_queue_->pop());
         delete cleaning;
     }
     delete qs_queue_;
